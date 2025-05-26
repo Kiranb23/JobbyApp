cfd4901ccaa21f8a6fe661d0c2b91e380f34ddff
Kiranb23 <124168936+Kiranb23@users.noreply.github.com>
2025-05-26 17:19:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
-import eslint from 'vite-plugin-eslint'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,7 +11,7 @@ export default defineConfig({
   build: {
     outDir: 'build',
   },
-  plugins: [react(), eslint()],
+  plugins: [react()],
   test: {
     environment: 'jsdom',
     globals: true,
